3907e1c9299ec38929e88afef974912f5b8f9d61
hieunmhe171547 <hieubeo5d@gmail.com>
2023-10-02 16:45:28 +0700

    build the first sample API

---
+++ src/main/java/SpringBoot/Tutorial/student/StudentController.java
@@ -0,0 +1,16 @@
+package SpringBoot.Tutorial.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+    @GetMapping
+    public List<String> findAllStudents() {
+        return List.of("CapNguyen", "Hello World");
+    }
+}
